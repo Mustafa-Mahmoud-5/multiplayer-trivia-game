5568c5a9c847118d06d3ab948e971fcf7b1446f5
Mohab <mohab_sobhy@outlook.com>
2026-03-17 05:19:28 +0200

    json protocol 2

--- src/main/java/org/game/common/jsonProtocol/Test.java
+++ src/main/java/org/game/common/jsonProtocol/Test.java
@@ -3,6 +3,7 @@ package org.game.common.jsonProtocol;
 import java.util.UUID;
 
 import org.game.common.jsonProtocol.enums.QuestionDifficulty;
+import org.game.common.jsonProtocol.enums.QuestionsResponseStatus;
 
 import com.google.gson.Gson;
 
@@ -12,5 +13,22 @@ public class Test {
         Gson gson = new Gson();
         String json = gson.toJson(requestQuestions);
         System.out.println(json);
+
+
+        String respone = "{\"requestId\":\"123e4567-e89b-12d3-a456-426614174000\",\"status\":\"OK\",\"questions\":[{\"id\":1,\"text\":\"What is the capital of France?\",\"choices\":[\"Paris\",\"London\",\"Berlin\",\"Madrid\"],\"correctAnswer\":0}]}";
+
+
+        QuestionsResponse questionsResponse = gson.fromJson(respone, QuestionsResponse.class);
+        System.out.println("Request ID: " + questionsResponse.getRequestId());
+        System.out.println("Status: " + questionsResponse.getStatus());
+        if (questionsResponse.getStatus() == QuestionsResponseStatus.OK) {
+            System.out.println("Questions:");
+            for (var question : questionsResponse.getQuestions()) {
+                System.out.println(" - " + question.getText());
+                System.out.println("   Options: " + question.getChoices());
+                System.out.println("   Correct Answer Index: " + question.getCorrectAnswer());
+            }
+        }
+
     }
 }
---
+++ src/main/java/org/game/common/jsonProtocol/enums/QuestionsResponseStatus.java
@@ -0,0 +1,6 @@
+package org.game.common.jsonProtocol.enums;
+
+public enum QuestionsResponseStatus {
+    OK,
+    ERROR
+}
